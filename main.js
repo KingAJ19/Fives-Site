8dbecb20d1d28982c0bd41f6579a9e70c39a04bb
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-15 07:44:59 +0200

    changed the time

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 11, 2023 18:00:00").getTime();
+var countDownDate = new Date("Feb 18, 2023 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
