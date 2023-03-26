ab4a29d94ff649057bac6160e1107723e408c624
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-27 01:20:29 +0200

    time

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("March 26, 2023 18:00:00").getTime();
+var countDownDate = new Date("April 02, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
