dec0779162bded1d0153d44ce4a03b491641ba02
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-04-20 02:09:34 +0200

    a

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+  // Display the result in the element with id="demo" 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s "; 
 
