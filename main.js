d8b5ef37911346b78f91bea1e9236b461b8181af
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-04-27 00:02:38 +0200

    aye

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var countDownDate = new Date("April 29, 2023 16:00:00").getTime();
 
-// Update the count down every 1 second 
+// Update the count down every 1 second  
 var x = setInterval(function () {
 
   // Get today's date and time
@@ -10,10 +10,10 @@ var x = setInterval(function () {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo" 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
