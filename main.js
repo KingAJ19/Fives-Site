99b1e10d8d297226648fbb95179dd4e474e00600
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-07-07 08:10:01 +0200

    s

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var countDownDate = new Date("July 08, 2023 18:00:00").getTime();
 var x = setInterval(function () {
 
   // Get today's date and time
-  var now = new Date().getTime(); 
-
+  var now = new Date().getTime();  
+ 
   // Find the distance between now and the count down date  
   var distance = countDownDate - now;
 
